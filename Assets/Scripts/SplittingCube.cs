84a5a515b9d13a30721fea97225ba038326e22c3
stanislav2454 <StanislavViktorovich15@yandex.ru>
2025-07-22 14:28:44 +0100

    2 fix

--- Assets/Scripts/SplittingCube.cs
+++ Assets/Scripts/SplittingCube.cs
@@ -6,8 +6,8 @@ public class SplittingCube : MonoBehaviour
     [SerializeField] private float explosionRadius = 5f;
     [SerializeField] private GameObject _cube;
 
-    private float splitChance = 1f;
-
+    private float currentSplitChance = 1f;
+    private const float ChanceReductionFactor = 0.5f;
     private void Awake()
     {
         GetComponent<Renderer>().material.color = Random.ColorHSV();
@@ -15,10 +15,15 @@ public class SplittingCube : MonoBehaviour
 
     private void OnMouseDown()
     {
-        if (Random.value <= splitChance)
+        if (Random.value <= currentSplitChance)
+        {
             SplitCube();
+            currentSplitChance *= ChanceReductionFactor;
+        }
         else
+        {
             Destroy(gameObject);
+        }
     }
 
     private void SplitCube()
@@ -26,20 +31,18 @@ public class SplittingCube : MonoBehaviour
         const int MinCubesCount = 2;
         const int MaxCubesCount = 6;
 
-        int newCubesCount = Random.Range(MinCubesCount, MaxCubesCount - 1);
+        int newCubesCount = Random.Range(MinCubesCount, MaxCubesCount + 1);
         GameObject[] newCubes = new GameObject[newCubesCount];
         Vector3 originalPosition = transform.position;
 
         CreateCubes(newCubesCount, newCubes, originalPosition);
-
         ExplodeObjects(newCubes, originalPosition);
-
         Destroy(gameObject);
     }
 
     private void CreateCubes(int newCubesCount, GameObject[] newCubes, Vector3 originalPosition)
     {
-        const float divider = 2f;
+        const float Divider = 2f;
         const float InstanceRadius = 1.5f;
 
         Vector3 originalScale = transform.localScale;
@@ -47,7 +50,7 @@ public class SplittingCube : MonoBehaviour
         for (int i = 0; i < newCubesCount; i++)
         {
             GameObject newCube = Instantiate(_cube);
-            newCube.transform.localScale = originalScale / divider;
+            newCube.transform.localScale = originalScale / Divider;
             newCube.transform.position = originalPosition + new Vector3(0, 1, 0) + Random.insideUnitSphere * InstanceRadius;
 
             newCube.GetComponent<Renderer>().material.color = Random.ColorHSV();
